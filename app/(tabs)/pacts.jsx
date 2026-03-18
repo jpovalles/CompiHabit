0d0f7ddfe3cef9d91059eb5a873b497cb7fee03b
jpovalles <jpovalles@javerianacali.edu.co>
2026-03-18 17:53:49 -0500

    Implement authentication flow with user registration, login, and logout features; add error handling for duplicate usernames and session management

--- app/(tabs)/pacts.jsx
+++ app/(tabs)/pacts.jsx
@@ -1,11 +1,23 @@
+import { AuthButton } from '@/components/AuthButton';
 import { Text, View } from 'react-native';
+import { useAuth } from '../../context/AuthContext';
 
 export default function pacts(){
+    const {signOut} = useAuth()
+
+    const handleLogout = async () => {
+        try{
+            await signOut()
+        }catch(error){
+            Alert.alert('Error', error.message)
+        }
+    }
     return(
         <View style={{flex:1, alignItems:'center'}}>
             <Text>
                 Holaaaaaaaaaaaaaaaa
             </Text>
+            <AuthButton label="Cerrar sesión" onPress={handleLogout}/>
         </View>
     )
 }
---
+++ context/error_msg.js
@@ -0,0 +1,4 @@
+export const error_msg = {
+    useAuthOut: "useAuth debe usarse dentro de AuthProvider",
+    duplicateUsername: 'El nombre de usuario ya está en uso',
+}
